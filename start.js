13b4d18e575c795e3a4970c5c43754bb5ad53ab6
shota-kuwata <99848619+shota-kuwata@users.noreply.github.com>
2022-03-30 08:10:41 +0900

    Update start.js

--- start.js
+++ start.js
@@ -2,12 +2,10 @@
 // 1行目に記載している 'use strict' は削除しないでください
 
 window.onload = function() {
+  console.log("start.js");
   const btn1 = document.getElementById("btn1");
   const iframe1 = document.parent.getElementById("iframe1");
   btn1.onclick = btn1Click;
-  
-  
-  
   
   function btn1Click() {
         iframe1.src = "./game.html";
