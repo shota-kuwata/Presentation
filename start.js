b041ffa5692b75e03b246e9400a1bc4755fe6caf
shota-kuwata <99848619+shota-kuwata@users.noreply.github.com>
2022-03-30 08:33:19 +0900

    Update start.js

--- start.js
+++ start.js
@@ -1,15 +1,17 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-function pageInit() {
-    console.log("start.js");
-    console.log(window);
-    let btn1 = window.document.getElementById("btn1");
-    btn1.onclick = btn1Click;
-    
-    function btn1Click() {
-        console.log("click");
-        window.parent.loadGameWindow();
+window.onload = function() {
+    function pageInit() {
+        console.log("start.js");
+        console.log(window);
+        let btn1 = window.document.getElementById("btn1");
+        btn1.onclick = btn1Click;
+
+        function btn1Click() {
+            console.log("click");
+            window.parent.loadGameWindow();
+        }
     }
 }
 
