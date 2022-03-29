dfbbf9993c5238caaac894006a222be00a0fb8c6
shota-kuwata <99848619+shota-kuwata@users.noreply.github.com>
2022-03-30 08:29:39 +0900

    Update start.js

--- start.js
+++ start.js
@@ -3,12 +3,12 @@
 
 pageInit() {
   console.log("start.js");
-  console.log(document);
-  let btn1 = document.getElementById("btn1");
+  console.log(window);
+  let btn1 = window.document.getElementById("btn1");
   btn1.onclick = btn1Click;
   
   function btn1Click() {
     console.log("click");
-    document.parent.loadGameWindow();
+    window.parent.loadGameWindow();
   }
 }
