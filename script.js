e5dc9c59205d2a7ca81c86de005fb8ae9f5aed6b
shota-kuwata <99848619+shota-kuwata@users.noreply.github.com>
2022-03-29 17:52:39 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+window.onload = function() {
+    console.log("onload");
+    let btn1;
+    const iframe1 = document.getElementById("iframe1");
+
+    console.log(iframe1.contentWindow.document)
+    if (iframe1.contentWindow.document.title === "スタート画面") {
+        console.log("スタート画面")
+        btn1 = document.getElementById("btn1");
+        btn1.onclick = btn1Click;
+    }
+
+
+    
+
+    function btn1Click() {
+        iframe1.src = "./game.html";
+        console.log("click");
+    }
+};
